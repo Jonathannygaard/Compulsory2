5fbb5c7d17185e75a2dd6c1f8ef19b44491f0f04
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2023-09-24 17:56:18 +0200

    Added bouble sort

--- Compulsory2/Compulsory2/Compulsory2.cpp
+++ Compulsory2/Compulsory2/Compulsory2.cpp
@@ -1,6 +1,53 @@
+#include <vector>
+#include <iostream>
+#include <chrono>
 
+std::vector<int> Vector;
+
+void create_vector(std::vector <int> v)
+{
+    for (int i = 0; i < 10000; i++)
+    {
+        v.push_back(rand() % 10000);
+    }
+    Vector = v;
+}
+
+void bubblesort(std::vector <int> v)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < v.size() - 1; i++)
+    {
+        swapped = false;
+        for (j = 0; j < v.size() - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                std::swap(v[j], v[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+    Vector = v;
+}
+
+void print_vector(std::vector <int> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << std::endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    std::srand(std::time(nullptr));
+    create_vector(Vector);
+    bubblesort(Vector);
+    print_vector(Vector);
     return 0;
 }
+
