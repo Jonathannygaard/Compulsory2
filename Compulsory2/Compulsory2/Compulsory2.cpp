087eb85f233fc915c686571c4d0fb2072a4fdaec
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2023-09-25 08:26:47 +0200

    Switched selection sort with merge sort

--- Compulsory2/Compulsory2/Compulsory2.cpp
+++ Compulsory2/Compulsory2/Compulsory2.cpp
@@ -4,6 +4,7 @@
 
 std::vector<int> Vector;
 void swap(std::vector<int>& v, int x, int y);
+int Sorting;
 
 void create_vector(std::vector <int> v)
 {
@@ -34,6 +35,13 @@ void bubblesort(std::vector <int> v)
     Vector = v;
 }
 
+void swap(std::vector<int>& v, int x, int y) {
+    int temp = v[x];
+    v[x] = v[y];
+    v[y] = temp;
+
+}
+
 void quicksort(std::vector<int> &vec, int L, int R) {
     int i, j, mid, piv;
     i = L;
@@ -62,13 +70,56 @@ void quicksort(std::vector<int> &vec, int L, int R) {
     }
 }
 
-void swap(std::vector<int>& v, int x, int y) {
-    int temp = v[x];
-    v[x] = v[y];
-    v[y] = temp;
+void merge(std::vector<int>& vec, int L, int M, int R) {
+    int i, j, k;
+    int n1 = M - L + 1;
+    int n2 = R - M;
 
+    std::vector<int> L_vec(n1);
+    std::vector<int> R_vec(n2);
+
+    for (i = 0; i < n1; i++)
+        L_vec[i] = vec[L + i];
+    for (j = 0; j < n2; j++)
+        R_vec[j] = vec[M + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = L;
+
+    while (i < n1 && j < n2) {
+        if (L_vec[i] <= R_vec[j]) {
+            vec[k] = L_vec[i];
+            i++;
+        }
+        else {
+            vec[k] = R_vec[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        vec[k] = L_vec[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        vec[k] = R_vec[j];
+        j++;
+        k++;
+    }
 }
 
+void merge_sort(std::vector<int>& vec, int L, int R) {
+    if (L < R) {
+        int M = (L + R) / 2;
+        merge_sort(vec, L, M);
+        merge_sort(vec, M + 1, R);
+        merge(vec, L, M, R);
+    }
+}
 void print_vector(std::vector <int> v)
 {
     for (size_t i = 0; i < v.size(); i++)
@@ -77,35 +128,30 @@ void print_vector(std::vector <int> v)
     }
 }
 
-void Selection_sort(std::vector <int> v)
-{
-    int min_index;
-    for (size_t i = 0; i < v.size() - 1; i++)
-    {
-        min_index = i;
-        for (size_t j = i + 1; j < v.size(); j++)
-        {
-            if (v[j] < v[min_index])
-            {
-                min_index = j;
-            }
-        }
-        std::swap(v[min_index], v[i]);
-    }
-    Vector = v;
-}
 
 int main(int argc, char* argv[])
 {
     std::srand(std::time(nullptr));
     create_vector(Vector);
+    std::cout << "Choose sorting algorithm: 1. BubbleSort 2. QuickSort 3. MergeSort" << std::endl;
+    std::cin >> Sorting;
     auto TimeStart = std::chrono::high_resolution_clock::now();
-    //bubblesort(Vector);
-    //quicksort(Vector,0,Vector.size()-1);
-    Selection_sort(Vector);
+    switch (Sorting)
+    {
+        case 1:
+            bubblesort(Vector);
+            break;
+        case 2:
+            quicksort(Vector, 0, Vector.size() - 1);
+            break;
+         default:
+            merge_sort(Vector, 0, Vector.size()-1);
+            break;
+    }
     auto TimeEnd = std::chrono::high_resolution_clock::now();
     print_vector(Vector);
     std::cout << "Time to sort:" << std::chrono::duration_cast<std::chrono::milliseconds>((TimeEnd) - (TimeStart)).count() << "ms" << std::endl; 
     return 0;
+    
 }
 
